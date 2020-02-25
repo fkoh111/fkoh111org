681307141a568326985fd562a759c879f4ace545
fkoh111 <frederik_kok@icloud.com>
2020-02-25 19:18:50 +0100

    Added state to splash component

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -2,19 +2,42 @@ import React, { Component } from "react";
 import ReactTypingEffect from "react-typing-effect";
 
 class Splash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
-    return (
-      <div>
-        ><span> </span>
-        <ReactTypingEffect
-          className="typingeffect"
-          text={["frederik kok hansen"]}
-          speed={100}
-          eraseDelay={5000}
-          cursor={"_"}
-        />
-      </div>
-    );
+    if (this.state.seconds <= 11.5) {
+      return (
+        <div>
+          <span>> </span>
+          <ReactTypingEffect
+            className="typingeffect"
+            text={["frederik kok hansen", "fkhonline.net"]}
+            speed={100}
+            eraseDelay={1300}
+            cursor={"_"}
+          />
+        </div>
+      );
+    } else {
+      return <div>> fkhonline.net_</div>;
+    }
   }
 }
 
